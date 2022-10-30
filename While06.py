64f43676209fd72a7a84d7c38daafa6ae614701b
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-10-30 22:34:55 +0300

    6

--- While06.py
+++ While06.py
@@ -7,3 +7,14 @@ def main(s):
     Returns:
         int: return answer
     """
+    k=0
+    i=0
+    while i<len(s):
+        if str(s[i]).isalpha():
+            if str(s[i])=='a' or str(s[i])=='e' or str(s[i])=='i' or str(s[i])=='o' or str(s[i])=='u' or str(s[i])=='A' or str(s[i])=='E' or str(s[i])=='I' or str(s[i])=='O' or str(s[i])=='U':
+                k=k+0
+            else:
+                k=k+1
+        i=i+1
+    return k
+print(main('CodeschoolUz'))
